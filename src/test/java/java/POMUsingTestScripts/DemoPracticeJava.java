2829f21ba0e777a52964733786d7fbb2105db572
RadhaPardeshi <radhadpardeshi@gmail.com>
2024-04-19 17:50:12 +0530

    First Commit

---
+++ src/test/java/java/POMUsingTestScripts/DemoPracticeJava.java
@@ -0,0 +1,10 @@
+package java.POMUsingTestScripts;
+
+public class DemoPracticeJava {
+	public static void main(String[] args) {
+		System.out.println("Main start");
+		System.out.println("Welcome to banking project");
+		System.out.println("Main end");
+	}
+
+}
